2f18a11d3baa148dba470a3d49dfc0dacffae063
Mohammed Kaif <80053728+kaifcodeadict@users.noreply.github.com>
2022-02-04 21:02:40 +0530

    1 mern

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Post() {
+  return (<>
+  </>);
+}
+
+export default Post;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Post from './Post/Post';
+
+function Posts() {
+  return (<>
+  <h1>posts</h1>
+  <Post/>
+  </>);
+}
+
+export default Posts;
